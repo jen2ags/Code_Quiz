d670415f38b1a25f3047874fce205a8a3e4861dc
jennj <jennjenn93@gmail.com>
2022-03-06 14:21:08 -0600

    results

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,7 +22,7 @@ var countdownTimer = document.querySelector(".timer .seconds")
 
     { 
         question: "2. Where us the correct place to insert a JavaScript?",
-        answer: "The <body> section",
+        answer: "The body section",
         options: [
             "The head section",
             "Both the head section and the body section are correct",
@@ -76,8 +76,12 @@ startButton.onclick = function() {
 
 let questionCount = 0;
 let timer;
+let score = 0;
 
 var nextButton = quizQuest.querySelector(".next");
+var results = document.querySelector(".results");
+var tryAgain = results.querySelector(".end-buttons .try-again");
+var quitQuiz = results.querySelector(".end-buttons .quit");
 
 //when the next button is clicked, the next question and answer choices will show up until the last question is shown
 nextButton.onclick = function() {
@@ -87,6 +91,7 @@ nextButton.onclick = function() {
     }
     else {
         console.log("Questions completed");
+        showResults();
     }
 }
 
@@ -114,10 +119,13 @@ function choicesSelected(answer) {
     let correct = questions[questionCount].answer;
     let options = optionsList.children.length;
     if(selection === correct) {
+        score += 1;
         answer.classList.add("correct");
     console.log("Answer is correct");
     }
+    //if the question was incorrect, it will notify you it was wrong
     else {
+        score += 0;
         answer.classList.add("incorrect");
         console.log("Answer is wrong");
     }
@@ -136,19 +144,39 @@ function startTimer(timeLeft) {
     function timer() {
         countdownTimer.textContent = timeLeft;
         timeLeft--;
+        if(timeLeft < 0) {
+            countdownTimer.textContent = "00";
+        }
+    
+    }
+}
+//the user's answers will be recorded as correct or incorrect and a score will be displayed 
+function showResults() {
+    //hide the quiz questions
+    quizQuest.classList.remove("activeQuiz");
+    //show the results
+    results.classList.add("activeResults");
+    var scoreNum = results.querySelector(".score");
+    if(score >= 0) {
+        var displayScore = '<span>You got a <p>'+ score +'</p> out of <p>'+ questions.length +'</p></span>';
+        scoreNum.innerHTML =displayScore;
     }
 }
 
+quitQuiz.onclick = function() {
+    window.location.reload();
+}
 
 
-
-
+tryAgain.onclick = function() {
+    window.location.reload();
+}
 
 //if the question was correct, it will move on to the next question with out penalty
-//if the question was incorrect, it will notify you it was wrong
+
 //the timer will deduct 10 seconds and the next question will show
 //after question 5, the quiz will end and the timer will stop
-//the user's answers will be recorded as correct or incorrect and a score will be displayed 
+
 //after the last question, the user's score is displayed on the screen and they will need to input their initials
 //the user's data will be saved to local storage in the quiz and store all the high scores
 
